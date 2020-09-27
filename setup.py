824dbf9c41f162c59548bc04f3dea4cd60a2795e
Varuna Jayasiri <vpjayasiri@gmail.com>
2020-09-27 18:11:53 +0530

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.rst", "r") as f:
 
 setuptools.setup(
     name='labml_helpers',
-    version='0.4.4',
+    version='0.4.5',
     author="Varuna Jayasiri, Nipun Wijerathne",
     author_email="vpjayasiri@gmail.com, hnipun@gmail.com",
     description="A collection of classes and functions to automate common deep learning training patterns",
